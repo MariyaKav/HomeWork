64626e2f23de9c5b9e798de3462cec62f0e15dde
Mariya Kaverina <kaverina.ma@mail.ru>
2024-02-08 02:09:08 +0400

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 3: Задайте произвольный массив. 
+//Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+
+int[] Array = { 2, 4, 6, 8, 10 };
+
+void OutputInRevers(int[] Array, int i)
+{
+    Console.Write(Array[i] + " ");
+
+    if (i > 0)
+    {
+        OutputInRevers(Array, i - 1);
+
+    }
+}
+
+Console.Write("Элементы массива в обратном порядке: ");
+
+OutputInRevers(Array,Array.Length-1);
